4ce082e722166545a5734152a953baca7ee99792
TAsenova <tasenova@mentormate.com>
2024-08-22 15:16:29 +0300

    simple routing

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,13 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/">Events</RouterLink> |
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
     </header>
   </div>
+  <!-- Outlet in React -->
   <RouterView />
 </template>
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>A site for various events</h1>
   </div>
 </template>
 
